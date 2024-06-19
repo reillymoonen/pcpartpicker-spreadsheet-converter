74dc0780518962ecae391bc70307668b96129f1e
reillymoonen <135081213+reillymoonen@users.noreply.github.com>
2024-06-20 11:59:31 +1200

    Implemented the loading bar into the main file

--- 00_pcpp_spreadsheet_converter.py
+++ 00_pcpp_spreadsheet_converter.py
@@ -1,6 +1,28 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from tqdm import tqdm
+import time
+from colorama import Fore, Style, init
+
+# implemented from https://www.geeksforgeeks.org/progress-bars-in-python/ and the tqdm GitHub documentation
+# Initialize colorama
+init(autoreset=True)
+
+
+def load_with_tqdm(total_steps):
+    # Customize the bar format to integrate the percentage with the progress bar
+    bar_format = '{desc}: {percentage:.2f}%|{bar}|'
+
+    with tqdm(total=total_steps, desc=f'{Fore.LIGHTWHITE_EX}Loading{Fore.LIGHTWHITE_EX}', ncols=100,
+              bar_format=bar_format) as pbar:
+        for _ in range(total_steps):
+            time.sleep(0.01)  # Simulate some work being done
+            pbar.update(1)
+
+
+# Example usage
+load_with_tqdm(100)
 
 
 def fetch_pcpartpicker_list(url):
